d16195431618590d767e7b5c4550d8981f7c6489
Martin Hlaváč <hlavacm@hotmail.com>
2017-09-16 02:42:40 +0200

    Added resources links in About section

--- website/src/pages/about.js
+++ website/src/pages/about.js
@@ -13,7 +13,12 @@ const AboutPage = () => (
         <h2>Cíl</h2>
         <p>TODO</p>
         <h2>Použité datové sady</h2>
-        <p>TODO</p>
+        <ul>
+            <li><a href="http://data.mfcr.cz/cs/dataset/centralni-registr-administrativnich-budov" target="_blank">Centrální registr administrativních budov (MF)</a></li>
+            <li><a href="https://data.cssz.cz/documentation/prehled-o-celkovem-poctu-osvc-podle-okresu" target="_blank">OSVČ (ČSSZ)</a></li>
+            <li><a href="http://vdp.cuzk.cz" target="_blank">Registr územní identifikace, adres a nemovitostí (ČUZK)</a></li>
+            <li><a href="https://www.czso.cz/csu/czso/uchazeci-o-zamestnani-dosazitelni-a-podil-nezamestnanych-osob-podle-obci_090417" target="_blank">Uchazeči o zaměstnání dosažitelní a podíl nezaměstnaných osob podle obcí (ČZSO)</a></li>
+        </ul>
         <h2>Autoři:</h2>
         <ul>
             <li>Dalibor Jaroš</li>
